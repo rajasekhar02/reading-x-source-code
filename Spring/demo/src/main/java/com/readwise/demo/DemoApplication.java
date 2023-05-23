13bf82c41ffd371bb236b74445aaf7d469843a34
raja sekhar <raja02sekhar08@gmail.com>
2023-05-23 14:40:10 -0500

    feat: refactor bean config to seperate file

--- Spring/demo/src/main/java/com/readwise/demo/DemoApplication.java
+++ Spring/demo/src/main/java/com/readwise/demo/DemoApplication.java
@@ -2,19 +2,11 @@ package com.readwise.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.readwise.demo.json.Greeting;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@Bean
-	public Greeting defaultGreeting() {
-		return new Greeting("Hello, World");
 	}
 }
