088e4ca0bb7cb91b6b81708d57d950fdf61b1540
rp6kp <rp6kp@umsystem.edu>
2023-05-27 18:20:33 -0500

    feat: optimized inner loop by precalculating the sums

--- Complete Reference/Algorithms/maximumSubArray.js
+++ Complete Reference/Algorithms/maximumSubArray.js
@@ -4,12 +4,17 @@
  */
 var maxSubArray = function (nums) {
     let maxSum = Math.min(...nums)
+    let prefixSums = [0]
+    for (let i = 0; i < nums.length; i++) {
+        prefixSums.push(prefixSums[prefixSums.length - 1] + nums[i]);
+    }
     for (let len = 1; len < nums.length + 1; len++) {
-        for (let start = 0; start < nums.length; start++) {
-            let sum = 0;
-            for (let itr = start; itr < (start + len); itr++) {
-                sum += nums[itr]
-            }
+        for (let start = 1; start <= nums.length; start++) {
+            let sum = prefixSums[start + len - 1] - prefixSums[start - 1];
+            // for(let itr = start; itr<(start+len);itr++){
+            //     sum += nums[itr]
+            // }
+            console.log(prefixSums[start + len - 1] - prefixSums[start - 1])
             if (maxSum < sum) {
                 maxSum = sum
             }
