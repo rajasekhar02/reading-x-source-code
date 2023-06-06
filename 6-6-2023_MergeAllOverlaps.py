f5623ea83f3e6b4a312170110ccf007bf551b870
raja sekhar <raja02sekhar08@gmail.com>
2023-06-06 10:18:49 -0500

    feat: add mergeAllOverlaps with bug at line 21

---
+++ 6-6-2023_MergeAllOverlaps.py
@@ -0,0 +1,28 @@
+from typing import *
+
+
+def mergeOverlappingIntervals(arr: List[List[int]]) -> List[List[int]]:
+    # Write your code here.
+    currMergeList = 0
+
+    sorted(arr, key=lambda x: x[0])
+
+    for i in range(1, len(arr)):
+        a = arr[currMergeList][0]
+        b = arr[currMergeList][1]
+        c = arr[i][0]
+        d = arr[i][1]
+
+        if c > b:
+            currMergeList += 1
+            arr[currMergeList][0] = c
+            arr[currMergeList][1] = d
+            continue
+        if c < b:
+            if d > b:
+                arr[currMergeList][1] = d
+    return arr[: currMergeList + 1]
+
+
+Q = [[1, 4], [3, 3], [3, 3], [3, 3], [3, 4], [4, 4], [5, 5], [5, 5], [5, 6]]
+print(mergeOverlappingIntervals(Q))
