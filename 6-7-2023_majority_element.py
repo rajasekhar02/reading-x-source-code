acfdffac95b9cd58580a48a9cd63bc3167f8c0c4
raja sekhar <raja02sekhar08@gmail.com>
2023-06-07 15:22:57 -0500

    feat: add solution for majority_element using moore voting algorithm

--- 6-7-2023_majority_element.py
+++ 6-7-2023_majority_element.py
@@ -3,22 +3,18 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return self.dcMajority(nums, 0, len(nums) - 1)[0]
-
-    # does not work all the cases
-    def dcMajority(self, nums, start, end):
-        if start < end:
-            mid = (start + end) // 2
-            majorEle1 = self.dcMajority(nums, start, mid)
-            majorEle2 = self.dcMajority(nums, mid + 1, end)
-            if majorEle1[0] == majorEle2[0]:
-                return [majorEle1[0], max(majorEle1[1], majorEle2[1]) + 1]
-            elif majorEle2[1] > majorEle1[1]:
-                return majorEle2
+        count = 0
+        element = 0
+        for i in nums:
+            if count == 0:
+                count = 1
+                element = i
+            elif element == i:
+                count += 1
             else:
-                return majorEle1
-        return [nums[start], 0]
+                count -= 1
+        return element
 
 
-nums = [6, 5, 5]
+nums = [3, 2, 3]
 print(Solution().majorityElement(nums))
