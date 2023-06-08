4e5d7e868d782f921a06fc93374136831c5d04af
raja sekhar <raja02sekhar08@gmail.com>
2023-06-08 09:47:59 -0500

    feat: dp in top down

--- 6-8-2023_leetcode_62.py
+++ 6-8-2023_leetcode_62.py
@@ -1,13 +1,33 @@
 class Solution:
+    def __init__(self):
+        self.dp = []
+
+    def initializeDP(self, m, n):
+        for i in range(0, m):
+            lis = []
+            for j in range(0, n):
+                lis.append(-1)
+            self.dp.append(lis)
+
     def uniquePaths(self, m: int, n: int) -> int:
-        return self.recur(m - 1, n - 1)
+        self.initializeDP(m, n)
+        return self.recurDP(m - 1, n - 1)
 
     def recur(self, posm, posn):
-        if posm == 0:
-            return 1
-        if posn == 0:
+        if posm == 0 or posn == 0:
             return 1
         return self.recur(posm - 1, posn) + self.recur(posm, posn - 1)
+
+    def recurDP(self, posm, posn):
+        if posm == 0 or posn == 0:
+            self.dp[posm][posn] = 1
+        elif self.dp[posm][posn] > -1:
+            return self.dp[posm][posn]
+        else:
+            self.dp[posm][posn] = self.recur(posm - 1, posn) + self.recur(
+                posm, posn - 1
+            )
+        return self.dp[posm][posn]
 
 
 m = 3
