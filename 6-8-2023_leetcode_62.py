4564161d51ea45596e5805c1585ec2192dea757a
raja sekhar <raja02sekhar08@gmail.com>
2023-06-08 11:15:53 -0500

    feat: add optimal dp solution

--- 6-8-2023_leetcode_62.py
+++ 6-8-2023_leetcode_62.py
@@ -10,8 +10,12 @@ class Solution:
             self.dp.append(lis)
 
     def uniquePaths(self, m: int, n: int) -> int:
+        self.initializeDP(2, n)
+        return self.optimalSol(m - 1, n - 1)
+
+    def subOptimaluniquePath(self, m: int, n: int) -> int:
         self.initializeDP(m, n)
-        return self.recurDP(m - 1, n - 1)
+        return self.iterDP(m - 1, n - 1)
 
     def recur(self, posm, posn):
         if posm == 0 or posn == 0:
@@ -29,7 +33,31 @@ class Solution:
             )
         return self.dp[posm][posn]
 
+    def iterDP(self, posm, posn):
+        self.dp[0][0] = 0
+        for i in range(0, posm + 1):
+            for j in range(0, posn + 1):
+                if i == 0:
+                    self.dp[i][j] = 1
+                elif j == 0:
+                    self.dp[i][j] = 1
+                else:
+                    self.dp[i][j] = self.dp[i - 1][j] + self.dp[i][j - 1]
+        return self.dp[posm][posn]
 
-m = 3
-n = 7
+    def optimalSol(self, posm, posn):
+        # self.dp[0][0] = 0
+        for j in range(0, posn + 1):
+            self.dp[0][j] = 1
+        self.dp[1][0] = 1
+        for it in range(0, posm):
+            for j in range(1, posn + 1):
+                self.dp[1][j] = self.dp[0][j] + self.dp[1][j - 1]
+            for j in range(0, posn + 1):
+                self.dp[0][j] = self.dp[1][j]
+        return self.dp[0][posn] if posm == 0 else self.dp[1][posn]
+
+
+m = 2
+n = 4
 print(Solution().uniquePaths(m, n))
