024420f472cbc3466c16f40857ff2108203f38c8
rp6kp <rp6kp@umsystem.edu>
2023-06-15 21:45:37 -0500

    feat: add solution for trapping water

--- 6-14-2023_leetcode_42.py
+++ 6-14-2023_leetcode_42.py
@@ -4,19 +4,39 @@ from typing import List
 class Solution:
     def trap(self, height: List[int]) -> int:
         total_area = 0
-        for i in range(0, len(height) - 2):
-            right_max_height = height[i + 1]
-            right_max_height_index = i + 1
-            block_occupied = 0
-            for j in range(i + 2, len(height)):
-                if right_max_height < height[j]:
-                    right_max_height = height[j]
-                    right_max_height_index = j
-                block_occupied += height[j]
-            total_area = (right_max_height_index - i) * min(height[i], right_max_height)
-            i = right_max_height_index - 1
+        left = 0
+        right = len(height) - 1
+        maxLeft = 0
+        maxRight = 0
+        while left <= right:
+            if height[left] <= height[right]:
+                if height[left] > maxLeft:
+                    maxLeft = height[left]
+                else:
+                    total_area += maxLeft - height[left]
+                left += 1
+            else:
+                if height[right] > maxRight:
+                    maxRight = height[right]
+                else:
+                    total_area += maxRight - height[right]
+                right -= 1
         return total_area
 
 
-h = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+h = [
+    10,
+    0,
+    8,
+    0,
+    6,
+    0,
+    4,
+    0,
+    6,
+    0,
+    8,
+    0,
+    10,
+]  # [0,1,0,2,0,3,0,4,0,3,0,2,0,1]#[4, 2, 0, 3, 2, 5]  # [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 print(Solution().trap(h))
