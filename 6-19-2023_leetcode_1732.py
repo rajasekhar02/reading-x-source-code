173149031568ac8abb6247a3593a129d1d6e9a32
rp6kp <rp6kp@umsystem.edu>
2023-06-19 09:50:18 -0500

    feat: add bug version

--- 6-19-2023_leetcode_1732.py
+++ 6-19-2023_leetcode_1732.py
@@ -3,10 +3,11 @@ from typing import List
 
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
+        maxHeight = 0
         currHeight = gain[0]
-        maxHeight = max(0, gain[0])
+        # Bug: Misses the last height from finding the maximum height
         for i in range(1, len(gain)):
-            currHeight += gain[i]
             if currHeight > maxHeight:
                 maxHeight = currHeight
+            currHeight += gain[i]
         return maxHeight
