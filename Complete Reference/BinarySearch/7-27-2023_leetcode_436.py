b3e058d291398d4c708c6a4f3cc2639cb45b1a75
Raja Sekhar Pothina <raja02sekhar08@gmail.com>
2023-07-27 16:53:43 -0500

    Update 7-27-2023_leetcode_436.py

--- Complete Reference/BinarySearch/7-27-2023_leetcode_436.py
+++ Complete Reference/BinarySearch/7-27-2023_leetcode_436.py
@@ -16,7 +16,7 @@ class Solution:
         high = end
         ans = -1
         while low <= high:
-            # able the below formula able to starting searching from given low index
+            # with the below formula able to starting searching from given low index
             mid = low + ((high-low) // 2)
             jIntervalStart = intervals[mid][0][0]
             if  jIntervalStart >= ithIntervalEnd:
