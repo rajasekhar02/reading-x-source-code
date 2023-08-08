44aeac11e8ebdab11fe9903ce3771a616c491153
raja sekhar <raja02sekhar08@gmail.com>
2023-08-08 16:28:36 -0500

    feat: add changes

--- rxjs/index.ts
+++ rxjs/index.ts
@@ -1,13 +1,29 @@
 import * as Rx from 'rxjs';
 const nextButton = document.getElementById('next');
 const backButton = document.getElementById('back');
-const subSelect = document.getElementById('sub');
+const subSelect: HTMLElement = document.getElementById('sub');
 const img = document.getElementById('img');
 const loading = document.getElementById('loading');
 
+// stackblitz
 const LOADING_ERROR_URL =
   'https://jhusain.github.io/reddit-image-viewer/error.png';
 const Observable = Rx.Observable;
+
+const subSelectChange = Rx.concat(
+  Rx.of(subSelect.value),
+  Rx.fromEvent(subSelect, 'change').pipe(
+    Rx.map((event: Event) => {
+      console.log(event.target.value);
+      return event.target.value;
+    })
+  )
+);
+
+const nextButtonClick = Rx.fromEvent(nextButton, 'click');
+const backButtonClick = Rx.fromEvent(backButton, 'click');
+
+const actions = new Observable();
 
 // function which returns an array of image URLs for a given reddit sub
 // getSubImages("pics") ->
@@ -55,19 +71,37 @@ function getSubImages(sub) {
 // ---------------------- INSERT CODE  HERE ---------------------------
 // This "images" Observable is a dummy. Replace it with a stream of each
 // image in the current sub which is navigated by the user.
-const images = Rx.of(
-  'https://upload.wikimedia.org/wikipedia/commons/3/36/Hopetoun_falls.jpg'
+loading.style.visibility = 'visible';
+
+const currPosition = Rx.merge(
+  nextButtonClick.pipe(Rx.map(() => 1)),
+  backButtonClick.pipe(Rx.map(() => -1))
+).pipe(Rx.scan((acc, curr) => acc + curr, 0));
+
+const images = subSelectChange.pipe(
+  Rx.map((sub) =>
+    getSubImages(sub).pipe(
+      (imagesArr) => currPosition.pipe(Rx.map((index) => imagesArr[index])),
+      Rx.switchAll()
+    )
+  ),
+  Rx.switchAll()
 );
+
+//  Rx.of(
+//   'https://upload.wikimedia.org/wikipedia/commons/3/36/Hopetoun_falls.jpg'
+// );
 
 images.subscribe({
   next(url) {
     // hide the loading image
     loading.style.visibility = 'hidden';
-
+    console.log(url);
     // set Image source to URL
     img.src = url;
   },
   error(e) {
+    console.error(e);
     alert(
       "I'm having trouble loading the images for that sub. Please wait a while, reload, and then try again later."
     );
@@ -77,16 +111,54 @@ images.subscribe({
 // This "actions" Observable is a placeholder. Replace it with an
 // observable that notfies whenever a user performs an action,
 // like changing the sub or navigating the images
-const actions = new Observable();
 
 actions.subscribe(() => (loading.style.visibility = 'visible'));
 
-
 // Rx.concat(
-//   Rx.of(1),
-//   Rx.filter(() => false),
-//   Rx.of(-1)
-// ).subscribe((v) => v);
-/*
-
-*/
+//   windowLoad.pipe(Rx.takeUntil(Rx.fromEvent(subSelect, 'change'))),
+//   subSelectChange
+// ).pipe(
+//   Rx.concatMap((selectedValue) => {
+//     return getSubImages(selectedValue);
+//   })
+// );
+// Rx.map(() =>
+//   subSelectChange.pipe(
+//     Rx.concatMap((selectedValue) => {
+//       loading.style.visibility = 'visible';
+//       // console.log(selectedValue);
+//       return getSubImages(selectedValue);
+//     }),
+//     Rx.mergeMap((imageUrls: string[]) => {
+//       currPosition = 0;
+//       let newImageUrls = imageUrls;
+//       return Rx.merge(
+//         Rx.of(newImageUrls[0]),
+//         backButtonClick.pipe(
+//           Rx.map(() => {
+//             if (currPosition == 0) {
+//               return newImageUrls[0];
+//             }
+//             currPosition--;
+//             return newImageUrls[currPosition];
+//           })
+//         ),
+//         nextButtonClick.pipe(
+//           Rx.map(() => {
+//             if (currPosition == newImageUrls.length - 1)
+//               return newImageUrls[currPosition];
+//             currPosition++;
+//             return newImageUrls[currPosition];
+//           })
+//         )
+//       ).pipe(
+//         Rx.map((imageUrl) => {
+//           if (imageUrl.endsWith('.png') || imageUrl.endsWith('.jpg')) {
+//             return imageUrl;
+//           }
+//           return LOADING_ERROR_URL;
+//         })
+//       );
+//     })
+//   )
+// );
