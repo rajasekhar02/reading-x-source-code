810cdd53021b4641c730dd5d50dbb3451692dfd1
raja sekhar <raja02sekhar08@gmail.com>
2023-08-18 08:07:26 -0500

    feat: add changes

--- src/chapter3.ts
+++ src/chapter3.ts
@@ -185,3 +185,18 @@ const statsPair = async (fileName: string) => {
 //         const pairs = await 
 //     }
 // }
+
+import fsa from 'fs-extra-promise'
+import yaml from 'js-yaml'
+
+const test = async () => {
+    const raw = await fsa.readFileAsync('config.yml', 'utf-8')
+    console.log('inside test, raw text', raw)
+    const cooked = yaml.load(raw)
+    console.log('inside test, cooked configuration', cooked)
+    return cooked
+}
+
+const result = test()
+console.log('outside test, result is', result.constructor.name)
+result.then(something => console.log('outside test we have', something))
