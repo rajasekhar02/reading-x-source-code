1520da9d3b07deb130f0fd49e71ad1f24eed39c1
rp6kp <rp6kp@umsystem.edu>
2023-08-26 16:11:02 -0500

    feat: add working solution

--- Complete Reference/DynamicProgramming/8-25-2023_cses_array_description.py
+++ Complete Reference/DynamicProgramming/8-25-2023_cses_array_description.py
@@ -18,26 +18,62 @@ def recurse(array, pos, prev_value, upBnd, n):
     return allCombs
 
 
+def iterativeDP2():
+    [n, upBnd] = list(map(int, input().split(" ")))
+    array = list(map(int, input().split(" ")))
+    # print(recurse(array, 0, array[0], upBnd, n))
+    dp = []
+    for i in range(0, n):
+        dp.append([])
+        for j in range(0, upBnd + 1):
+            dp[i].append(0)
+    if array[n - 1] == 0:
+        for i in range(1, upBnd + 1):
+            dp[n - 1][i] = 1
+    else:
+        dp[n - 1][array[n - 1]] = 1
+    for pos in range(n - 2, -1, -1):
+        if array[pos] != 0:
+            # prev_value = array[n - 1] if pos == n - 1 else array[pos + 1]
+            for newValue in [array[pos] - 1, array[pos], array[pos] + 1]:
+                # newValue = possible_value + direction
+                if newValue >= 1 and newValue <= upBnd:
+                    dp[pos][array[pos]] += dp[pos + 1][newValue]
+                    dp[pos][array[pos]] %= 1000000007
+            continue
+        for possible_value in range(1, upBnd + 1):
+            for newValue in [
+                possible_value - 1,
+                possible_value,
+                possible_value + 1,
+            ]:
+                # newValue = possible_value + direction
+                if newValue >= 1 and newValue <= upBnd:
+                    dp[pos][possible_value] += dp[pos + 1][newValue]
+                    dp[pos][possible_value] %= 1000000007
+    print(dp, sum(dp[0]))
+
+
 if __name__ == "__main__":
     [n, upBnd] = list(map(int, input().split(" ")))
     array = list(map(int, input().split(" ")))
     # print(recurse(array, 0, array[0], upBnd, n))
     dp = []
-    for i in range(0, n + 1):
+    for i in range(0, n):
         dp.append([])
         for j in range(0, upBnd + 1):
-            dp[i].append(int(i == n))
-    print(dp)
+            dp[i].append(int(i == n - 1))
+    # print(dp)
     # dp[n][0] = upBnd
-    for pos in range(n - 1, -1, -1):
-        prev_value = array[n - 1] if pos == n - 1 else array[pos + 1]
-        # if array[pos] != 0:
-        #     dp[pos][prev_value] = (
-        #         dp[pos + 1][array[pos]]
-        #         + dp[pos + 1][array[pos] + 1]
-        #         + dp[pos + 1][array[pos] - 1]
-        #     )
-        #     continue
+    for pos in range(n - 2, -1, -1):
+        prev_value = array[pos]
+        if array[pos] != 0:
+            dp[pos][prev_value] += dp[pos + 1][array[pos]]
+            if array[pos] < upBnd:
+                dp[pos][prev_value] += dp[pos + 1][array[pos] + 1]
+            if array[pos] > 1:
+                dp[pos][prev_value] += dp[pos + 1][array[pos] - 1]
+            continue
         # start = 1 if prev_value <= 1 else (prev_value - 1)
         # end = upBnd if prev_value == 0 else min((prev_value + 1), upBnd)
         for possible_value in range(1, upBnd + 1):
@@ -46,4 +82,4 @@ if __name__ == "__main__":
                 dp[pos][possible_value] += dp[pos + 1][possible_value + 1]
             if possible_value > 1:
                 dp[pos][possible_value] += dp[pos + 1][possible_value - 1]
-    print(dp, dp[0][array[0]])
+    print(sum(dp[0]))
