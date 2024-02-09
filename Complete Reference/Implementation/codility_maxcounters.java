58c05eaf79b0c13f5111e62f0a8dd85c96cf8385
Raja Sekhar Pothina <raja02sekhar08@gmail.com>
2024-02-09 10:09:54 -0600

    Create codility_maxcounters.java

---
+++ Complete Reference/Implementation/codility_maxcounters.java
@@ -0,0 +1,41 @@
+// you can also use imports, for example:
+import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int[] solution(int N, int[] A) {
+        // Implement your solution here
+        int max_element = 0;
+        int max_val_at_max_indicator = 0;
+        int max_indicator = N+1;
+        boolean max_indicator_occurred = false;
+        int[] resultArr = new int[N];
+        Set<Integer> setOfVisitedNumbers = new HashSet<>();
+        for (int i=0;i<A.length;i++){
+            if(A[i] < max_indicator){
+                if(max_indicator_occurred && !setOfVisitedNumbers.contains(A[i]) ){
+                    resultArr[A[i]-1] = max_val_at_max_indicator + 1;
+                } else {
+                    resultArr[A[i]-1] += 1;
+                }
+                setOfVisitedNumbers.add(A[i]);
+                max_element = Math.max(resultArr[A[i]-1], max_element);
+            } else {
+                // System.out.println(Arrays.toString(resultArr));
+                max_indicator_occurred = true;
+                max_val_at_max_indicator = max_element;
+                setOfVisitedNumbers = new HashSet<>();
+            }
+        }
+
+        for(int i=0; i<N; i++){
+            if(!setOfVisitedNumbers.contains(i+1)){
+                resultArr[i] = max_val_at_max_indicator;
+            }
+        }
+        // System.out.println(Arrays.toString(resultArr));
+        return resultArr;
+    }
+}
